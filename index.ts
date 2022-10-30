ccb87c02795b2270f0f4047606a708f04d53dc7e
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-30 22:37:10 +0100

    restructure repo

--- index.ts
+++
@@ -1,8 +0,0 @@
-// Import stylesheets
-import './style.css';
-import { generateGrid } from './functions/generate';
-
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-
-appDiv.appendChild(generateGrid(81, 9));
